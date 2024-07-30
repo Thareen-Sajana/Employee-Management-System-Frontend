6a7e064a1ce3d26b113bd3cdaa689c972c40a9f7
Thareen-Sajana <thareensajana0@gmail.com>
2024-07-30 12:25:34 +0530

    Add the Post API for employee

--- src/app/page/manage-emp/manage-emp.component.ts
+++ src/app/page/manage-emp/manage-emp.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-manage-emp',
   standalone: true,
-  imports: [FormsModule,],
+  imports: [FormsModule,HttpClientModule,CommonModule],
   templateUrl: './manage-emp.component.html',
   styleUrl: './manage-emp.component.css'
 })
@@ -18,7 +20,12 @@ export class ManageEmpComponent {
     "roleId": undefined
   }
 
+  constructor(private http:HttpClient){}
+
   public addEmployee(){
     console.log(this.employeeObj)
+    this.http.post("http://localhost:8080/employee/add",this.employeeObj).subscribe(res => {
+      console.log("Done")
+    })
   }
 }
